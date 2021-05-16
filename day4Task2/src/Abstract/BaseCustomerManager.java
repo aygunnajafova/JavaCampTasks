386d7d2e748916b7c75408d4decf9268da706622
Aygün Nəcəfova <aygun.nacafova.577@teams.dia.edu.az>
2021-05-17 01:33:18 +0400

    interface/abstract

--- day4Task2/src/Abstract/BaseCustomerManager.java
+++ day4Task2/src/Abstract/BaseCustomerManager.java
@@ -6,7 +6,7 @@ public abstract class BaseCustomerManager implements CustomerService {
 
 	public void save(Customer customer) {
 		System.out.println("Saved to db : " + customer.getFirstName());
-
+		
 	}
 	
 }
