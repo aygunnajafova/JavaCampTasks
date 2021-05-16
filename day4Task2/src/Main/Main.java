051072e537632cc5d7eda9e5bb6ed226f2b61321
Aygün Nəcəfova <aygun.nacafova.577@teams.dia.edu.az>
2021-05-17 01:35:07 +0400

    interface/abstract

--- day4Task2/src/Main/Main.java
+++ day4Task2/src/Main/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
 		baseCustomerManager.save(new Customer(1,"Aygun","Najafova",LocalDate.of(2002,1,15),"0123456789"));
-
+		
 	}
 
 }
